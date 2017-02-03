8edea7acff38e4093e63f55a6375a23ba4448cad
Florian de Boissieu <fdeboiss@gmail.com>
2017-02-03 11:11:11 +0100

    changed lasfilter queries with lasreader fast AOI queries in readLAS src makes the extraction of rectangle/circles faster

--- src/readLAS.cpp
+++ src/readLAS.cpp
@@ -86,8 +86,10 @@ List lasdatareader(CharacterVector file, bool Intensity, bool ReturnNumber, bool
     // Initialize las objects
     LASreadOpener lasreadopener;
     lasreadopener.set_file_name(filechar);
+    lasreadopener.parse_str(filterchar);
     LASreader* lasreader = lasreadopener.open();
-    LASfilter lasfilter;
+    // LASfilter lasfilter;
+    
 
     if(0 == lasreader || NULL == lasreader)
       throw std::runtime_error("LASlib internal error. See message above.");
@@ -104,16 +106,16 @@ List lasdatareader(CharacterVector file, bool Intensity, bool ReturnNumber, bool
     // First pass to read the whole file. Aims to know how much memory we must allocate
     if(stream)
     {
-      lasfilter.parse_string(filterchar);
+//       lasfilter.parse_string(filterchar);
 
       npoints = 0;
       while (lasreader->read_point())
       {
-        if(!lasfilter.filter(&lasreader->point))
+//         if(!lasfilter.filter(&lasreader->point))
           npoints++;
       }
 
-      lasreader->seek(0);
+      lasreadopener.reopen(lasreader);
     }
 
     // Allocate the required amount of data only if necessary
@@ -142,8 +144,8 @@ List lasdatareader(CharacterVector file, bool Intensity, bool ReturnNumber, bool
     unsigned long int i = 0;
     while (lasreader->read_point())
     {
-      if(stream && lasfilter.filter(&lasreader->point))
-        continue;
+//       if(stream && lasfilter.filter(&lasreader->point))
+//         continue;
 
       X[i]   = lasreader->point.get_x();
       Y[i]   = lasreader->point.get_y();
