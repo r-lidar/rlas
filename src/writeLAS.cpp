eef8fdf17f4bf88b63e374a8335789ab12d35d7e
Jean-Romain <jromain.roussel@gmail.com>
2017-05-23 07:38:14 -0400

    Fix #4 - bug of double precision
    Former-commit-id: f570f0f0731943506b3229e33e1e7c421b0331df

--- src/writeLAS.cpp
+++ src/writeLAS.cpp
@@ -117,9 +117,9 @@ void laswriter(CharacterVector file,
 
     for(int i = 0 ; i < X.length() ; i++)
     {
-      p.set_X((X[i]-header.x_offset)/header.x_scale_factor );
-      p.set_Y((Y[i]-header.y_offset)/header.y_scale_factor );
-      p.set_Z((Z[i]-header.z_offset)/header.z_scale_factor );
+      p.set_x(X[i]);
+      p.set_y(Y[i]);
+      p.set_z(Z[i]);
 
       if(I.length() > 0){ p.set_intensity((U16)I[i]); }
       if(RN.length() > 0){ p.set_return_number((U8)RN[i]); }
