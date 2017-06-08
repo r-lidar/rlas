03b4b377a8f73a22991152ab9b9287797ac9ec39
Jean-Romain <jromain.roussel@gmail.com>
2017-06-08 13:01:14 -0400

    Add bash script to make LASlib R compliant

---
+++ laslib2R.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# The LASlib and LASzip are open source librairies developped by Martin Isenburg to read and
+# write las and laz file. I wrapped these libraries into R. However R does not like the way
+# LASLib and LASzip are coded. R does not like rand and srand and exit(1). Moreover and the
+# use of standard output (sdtout, stderr) are not correct in a library.
+
+# The following regex enable to automatically correct some problems:
+
+files=`grep -lr "fprintf" src/`
+
+perl -pi -w -e 's/fprintf\(stderr,/REprintf(/g;' $files
+
+files=`grep -lr "exit(1)" src/`
+
+perl -pi -w -e 's/exit\(1\)/throw std::runtime_error\(std::string\("Internal error"\)\)/g;' $files
+
+# Some extra changes were done manually:
+
+# fopen_compressed.cpp                #include <R.h> and #define STRICT_R_HEADERS
+# mydefs.hpp                          #include <R.h> and #define STRICT_R_HEADERS
+# lasreaderpipeon.cpp      l96-101    comment lines
+# laswriter.cpp            l135-199   comment lines
+# bytestreamount_file.hpp  l134       return (true)
+# lastransform.cpp         l357       R::runif(0, RAND_MAX);
+# lasfilter.cpp            l1298      R::runif(0, RAND_MAX);
+# lasutility.cpp                      #include <stdexcept>
+
+# R is happy now!
+
