8721badbc4d0db6d960f22e3d22c41bdad7062a8
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2019-01-31 10:31:21 -0500

    Update

--- laslib2R.sh
+++ laslib2R.sh
@@ -20,9 +20,9 @@ perl -pi -w -e 's/exit\(1\)/throw std::runtime_error\("Internal error"\)/g;' $fi
 # lasreaderpipeon.cpp      l96-101    comment lines
 # laswriter.cpp            l135-199   comment lines
 # laswriter.cpp            l1037      for gcc 8+ replace strncpy by memcpy
-# lasattributer.hpp        l79-80     for gcc 8+ replace strncpy by memcpy - requieres to get the char* length:
-#                                     int len = 0 ; while(*(description+len) != '\0' && len < 32) len++;
-#                                     memcpy(this->description, description, len);
+# lasattributer.hpp        l79-80     for gcc 8+ replace strncpy by memcpy - requires to get the char* length:
+#     int len = 0 ; while(*(description+len) != '\0' && len < 32) len++;
+#     memcpy(this->description, description, len);
 # laswriter_bin.cpp        l176       for gcc 8+ replace strncpy by memcpy
 # bytestreamount_file.hpp  l134       return (true)
 # lastransform.cpp         l357       R::runif(0, RAND_MAX);
@@ -30,4 +30,7 @@ perl -pi -w -e 's/exit\(1\)/throw std::runtime_error\("Internal error"\)/g;' $fi
 # lasutility.cpp                      #include <stdexcept>
 
 # R is happy now!
+
+# In addition:
+# - Tiago added the support of a new filter: see issue #32
 
