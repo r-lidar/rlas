88ac6317963ea8e904010bf916d7a3b4e46e9e15
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2021-03-02 15:45:04 -0500

    update

--- laslib2R.sh
+++ laslib2R.sh
@@ -19,8 +19,8 @@ perl -pi -w -e 's/exit\(1\)/throw std::runtime_error\("Internal error"\)/g;' $fi
 
 # fopen_compressed.cpp                #include <R.h> and #define STRICT_R_HEADERS
 # mydefs.hpp                          #include <R.h> and #define STRICT_R_HEADERS
-# lasreaderpipeon.cpp      l96-101    comment lines
-# laswriter.cpp            l135-199   comment lines
+# lasreaderpipeon.cpp      l96-101    comment lines because of stdout
+# laswriter.cpp            l139-204   comment lines because of stdout (I guess)
 # laswriter.cpp            l1130,1135 for gcc 8+ replace strncpy by memcpy
 # lasattributer.hpp        l79-80     for gcc 8+ replace strncpy by memcpy - requires to get the char* length:
 #     int len = 0 ; while(*(description+len) != '\0' && len < 32) len++;
@@ -28,17 +28,18 @@ perl -pi -w -e 's/exit\(1\)/throw std::runtime_error\("Internal error"\)/g;' $fi
 # lasdefinition.hpp        l570       cast to void* : memset((void*)&(vlrs[i]), 0, sizeof(LASvlr));
 # lasdefinition.hpp        l572,697   for gcc 8+ replace strncpy by memcpy in the same way than lasattributer.hpp
 # laswriter_bin.cpp        l173       for gcc 8+ replace strncpy by memcpy
-# bytestreamount_file.hpp  l134       return (true)
-# lastransform.cpp         l401       R::runif(0, RAND_MAX);
-# lasfilter.cpp            l1298      R::runif(0, RAND_MAX);
+# bytestreamount_file.hpp  l134       return (true) to get rid of stdout
+# lastransform.cpp         l584       R::runif(0, RAND_MAX);
+# lasfilter.cpp            l1310      R::runif(0, RAND_MAX);
 # lasutility.cpp                      #include <stdexcept>
-# lasinterval.cpp          l545       delete ((LASintervalStartCell*)previous_cell); for gcc-asan fix in lax files
+# lasinterval.cpp          l552       delete ((LASintervalStartCell*)previous_cell); for gcc-asan fix in lax files
 # lasquadtree.cpp          l1662      for interger overflow
 #    if (l < 16)
 #      level_offset[l+1] = level_offset[l] + ((1<<l)*(1<<l));
 #    else
 #      level_offset[l+1] = level_offset[l];
 # bytestreamin_file.hpp    l153,166   off_t -> off64_t (see #50)
+# bytestreamout_file.hpp   l152,163   off_t -> off64_t (see #50)
 # CRAN is happy now!
 
 # In addition:
