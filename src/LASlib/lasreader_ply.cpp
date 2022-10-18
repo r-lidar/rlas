33269362aa92f1436199812fa97d0d1a5c9e7bd0
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2022-10-18 10:30:10 -0400

    fix -Wformat %x U32

--- src/LASlib/lasreader_ply.cpp
+++ src/LASlib/lasreader_ply.cpp
@@ -1500,7 +1500,7 @@ BOOL LASreaderPLY::parse(const char* parse_string)
     }
     else if (p[0] == 'J') // we expect a hexadecimal coded intensity
     {
-      I32 hex_value;
+      U32 hex_value;
       while (l[0] && (l[0] == ' ' || l[0] == ',' || l[0] == '\t' || l[0] == ';' || l[0] == '\"')) l++; // first skip white spaces and quotes
       if (l[0] == 0) return FALSE;
       sscanf(l,"%x",&hex_value);
