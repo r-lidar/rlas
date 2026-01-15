63553bcef3d2a5ea1c05bb1ee5d81f2b11e41cd1
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2026-01-15 12:48:43 -0500

    include string vector

--- src/altrep_compact_replication.cpp
+++ src/altrep_compact_replication.cpp
@@ -500,20 +500,24 @@ bool R_is_materialized(SEXP x)
   return DATAPTR_OR_NULL(x) != nullptr;
 }
 
+#if R_VERSION >= R_Version(4, 6, 0)
+#include <string>
+#include <vector>
+#endif
 
 // [[Rcpp::export]]
 SEXP R_altrep_full_class(SEXP x) {
   if (!ALTREP(x)) return R_NilValue;
-#if R_VERSION >= R_Version(4, 6, 0)
-  std::vector<std::string> v;
-  auto visitor = [](SEXP name, SEXP attr, void* data) -> SEXP {
-    std::vector<std::string>* ptr = static_cast<std::vector<std::string>*>(data);
-    std::string s{CHAR(Rf_asChar(attr))};
-    ptr->push_back(s);
-    return NULL;
-  };
-  return R_mapAttrib(ALTREP_CLASS(x), visitor, static_cast<void*>(&v));
-#else
-  return ATTRIB(ALTREP_CLASS(x));
-#endif
+  #if R_VERSION >= R_Version(4, 6, 0)
+    std::vector<std::string> v;
+    auto visitor = [](SEXP name, SEXP attr, void* data) -> SEXP {
+      std::vector<std::string>* ptr = static_cast<std::vector<std::string>*>(data);
+      std::string s{CHAR(Rf_asChar(attr))};
+      ptr->push_back(s);
+      return NULL;
+    };
+    return R_mapAttrib(ALTREP_CLASS(x), visitor, static_cast<void*>(&v));
+  #else
+    return ATTRIB(ALTREP_CLASS(x));
+  #endif
 }
